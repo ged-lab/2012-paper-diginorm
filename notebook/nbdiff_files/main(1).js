b7917ffb4d12ff149b982561909bac579ad78af3
Michael Wright <wrigh517@gmail.com>
2014-07-14 11:49:18 -0400

    Inclusion of nbdiff html notebook.

--- notebook/nbdiff_files/main(1).js
+++
@@ -1,7 +0,0 @@
-if (typeof IPython.notebook === 'undefined') {
-    $([IPython.events]).bind('notebook_loaded.Notebook', function () {
-        NBDiff.init();
-    });
-} else {
-    NBDiff.init();
-}
